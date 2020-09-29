f2bca75f645e4b10093ffe37aca23bea2469e45d
colin <colins110@hotmail.com>
2020-09-29 16:58:00 +0800

    fix syncpoint bug by ticker in changefeed.go

--- cdc/changefeed.go
+++ cdc/changefeed.go
@@ -725,31 +725,34 @@ func (c *changeFeed) calcResolvedTs(ctx context.Context) error {
 	}
 
 	//sync-point on
-	if c.info.SyncPointEnabled {
-		c.syncpointMutex.Lock()
-		defer c.syncpointMutex.Unlock()
-		if c.status.ResolvedTs == c.status.CheckpointTs && !c.updateResolvedTs {
-			log.Info("sync point reached by ticker", zap.Uint64("ResolvedTs", c.status.ResolvedTs), zap.Uint64("CheckpointTs", c.status.CheckpointTs), zap.Bool("updateResolvedTs", c.updateResolvedTs), zap.Uint64("ddlResolvedTs", c.ddlResolvedTs), zap.Uint64("ddlTs", c.ddlTs))
-			c.updateResolvedTs = true
-			err := c.sinkSyncpoint(ctx)
-			if err != nil {
-				log.Error("syncpoint sink fail", zap.Uint64("ResolvedTs", c.status.ResolvedTs), zap.Uint64("CheckpointTs", c.status.CheckpointTs), zap.Error(err))
-			}
-		}
+	// if c.info.SyncPointEnabled {
+	// 	c.syncpointMutex.Lock()
+	// 	defer c.syncpointMutex.Unlock()
+	// 	if c.status.ResolvedTs == c.status.CheckpointTs && !c.updateResolvedTs {
+	// 		c.updateResolvedTs = true
+	// 		if c.status.CheckpointTs != c.ddlResolvedTs || c.status.CheckpointTs <= c.ddlExecutedTs {
+	// 			log.Info("sync point reached by ticker", zap.Uint64("ResolvedTs", c.status.ResolvedTs), zap.Uint64("CheckpointTs", c.status.CheckpointTs), zap.Bool("updateResolvedTs", c.updateResolvedTs), zap.Uint64("ddlResolvedTs", c.ddlResolvedTs), zap.Uint64("ddlExecutedTs", c.ddlExecutedTs), zap.Uint64("ddlTs", c.ddlTs))
+	// 			err := c.sinkSyncpoint(ctx)
+	// 			if err != nil {
+	// 				log.Error("syncpoint sink fail", zap.Uint64("ResolvedTs", c.status.ResolvedTs), zap.Uint64("CheckpointTs", c.status.CheckpointTs), zap.Error(err))
+	// 			}
+	// 		}
 
-		if c.status.ResolvedTs == 0 {
-			c.updateResolvedTs = true
-		}
+	// 	}
 
-		if c.status.ResolvedTs == c.status.CheckpointTs && c.status.ResolvedTs == c.ddlTs && c.ddlTs <= c.ddlExecutedTs {
-			log.Info("sync point reached by ddl", zap.Uint64("ResolvedTs", c.status.ResolvedTs), zap.Uint64("CheckpointTs", c.status.CheckpointTs), zap.Bool("updateResolvedTs", c.updateResolvedTs), zap.Uint64("ddlResolvedTs", c.ddlResolvedTs), zap.Uint64("ddlTs", c.ddlTs), zap.Uint64("ddlExecutedTs", c.ddlExecutedTs))
-			err := c.sinkSyncpoint(ctx)
-			if err != nil {
-				log.Error("syncpoint sink fail", zap.Uint64("ResolvedTs", c.status.ResolvedTs), zap.Uint64("CheckpointTs", c.status.CheckpointTs), zap.Error(err))
-			}
-			c.ddlTs = 0
-		}
-	}
+	// 	if c.status.ResolvedTs == 0 {
+	// 		c.updateResolvedTs = true
+	// 	}
+
+	// 	if c.status.ResolvedTs == c.status.CheckpointTs && c.status.ResolvedTs == c.ddlTs && c.ddlTs <= c.ddlExecutedTs {
+	// 		log.Info("sync point reached by ddl", zap.Uint64("ResolvedTs", c.status.ResolvedTs), zap.Uint64("CheckpointTs", c.status.CheckpointTs), zap.Bool("updateResolvedTs", c.updateResolvedTs), zap.Uint64("ddlResolvedTs", c.ddlResolvedTs), zap.Uint64("ddlTs", c.ddlTs), zap.Uint64("ddlExecutedTs", c.ddlExecutedTs))
+	// 		err := c.sinkSyncpoint(ctx)
+	// 		if err != nil {
+	// 			log.Error("syncpoint sink fail", zap.Uint64("ResolvedTs", c.status.ResolvedTs), zap.Uint64("CheckpointTs", c.status.CheckpointTs), zap.Error(err))
+	// 		}
+	// 		c.ddlTs = 0
+	// 	}
+	// }
 
 	if len(c.taskPositions) < len(c.taskStatus) {
 		log.Debug("skip update resolved ts",
@@ -842,6 +845,34 @@ func (c *changeFeed) calcResolvedTs(ctx context.Context) error {
 	checkUpdateTs()
 
 	var tsUpdated bool
+
+	//syncpoint on
+	if c.info.SyncPointEnabled {
+		c.syncpointMutex.Lock()
+		defer c.syncpointMutex.Unlock()
+		if c.status.ResolvedTs == c.status.CheckpointTs && !c.updateResolvedTs && c.ddlTs == 0 {
+			log.Info("sync point reached by ticker", zap.Uint64("ResolvedTs", c.status.ResolvedTs), zap.Uint64("CheckpointTs", c.status.CheckpointTs), zap.Bool("updateResolvedTs", c.updateResolvedTs), zap.Uint64("ddlResolvedTs", c.ddlResolvedTs), zap.Uint64("ddlExecutedTs", c.ddlExecutedTs), zap.Uint64("ddlTs", c.ddlTs))
+			c.updateResolvedTs = true
+			err := c.sinkSyncpoint(ctx)
+			if err != nil {
+				log.Error("syncpoint sink fail", zap.Uint64("ResolvedTs", c.status.ResolvedTs), zap.Uint64("CheckpointTs", c.status.CheckpointTs), zap.Error(err))
+			}
+
+		}
+
+		if c.status.ResolvedTs == 0 {
+			c.updateResolvedTs = true
+		}
+
+		if c.status.ResolvedTs == c.status.CheckpointTs && c.status.ResolvedTs == c.ddlTs && c.ddlTs <= c.ddlExecutedTs {
+			log.Info("sync point reached by ddl", zap.Uint64("ResolvedTs", c.status.ResolvedTs), zap.Uint64("CheckpointTs", c.status.CheckpointTs), zap.Bool("updateResolvedTs", c.updateResolvedTs), zap.Uint64("ddlResolvedTs", c.ddlResolvedTs), zap.Uint64("ddlTs", c.ddlTs), zap.Uint64("ddlExecutedTs", c.ddlExecutedTs))
+			err := c.sinkSyncpoint(ctx)
+			if err != nil {
+				log.Error("syncpoint sink fail", zap.Uint64("ResolvedTs", c.status.ResolvedTs), zap.Uint64("CheckpointTs", c.status.CheckpointTs), zap.Error(err))
+			}
+			c.ddlTs = 0
+		}
+	}
 
 	//syncpoint on
 	if c.info.SyncPointEnabled {
